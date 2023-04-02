0e7280563f286db35d9a013e7673feb06854b178
23nosurrend <keynesbizimana@gmail.com>
2023-04-02 12:42:41 +0200

    this is for practice of setTimeout

---
+++ scripts/practice.js
@@ -0,0 +1,21 @@
+
+const save=document.getElementById("save");
+save.addEventListener("click",()=>{
+
+console.log(x)
+
+})
+//THis new trial of set timeout
+
+
+function myfuction(){
+    let stop=setTimeout(function(){
+        alert("hellow wrold");
+    },2000)
+   
+};
+const duration=document.getElementById("time");
+duration.addEventListener("click",fuction(){
+    clearTimeout(stop)
+});
+
